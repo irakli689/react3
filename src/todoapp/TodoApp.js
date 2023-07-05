afe714abcaabe8610023f6a7198aaf0fbb0c4153
irakli689 <irakli689@gmail.com>
2023-07-06 02:28:39 +0400

    ...

--- src/todoapp/TodoApp.js
+++ src/todoapp/TodoApp.js
@@ -1,5 +1,7 @@
-function TodoApp(){
-    const ITEMS=[
+import classNames from "classnames"
+import { useState } from "react";
+
+const ITEMS=[
         {
             id: 1,
             title: 'lorem ipsum 1',
@@ -20,18 +22,58 @@ function TodoApp(){
             title: 'lorem ipsum 4',
             completed: true
         }
-    ]
-    return <div>
-        <ul>
-            {ITEMS.map(item=>{
-                return <li key={item.id} className={item.completed ? 'completed' : ''}>
-                    <input type="checkbox"/>
-                    {item.title}
-                    <button>delete</button>
-                </li>
-            })}
-        </ul>
-    </div>
+]
+
+function TodoApp(){
+    const [todo, setTodo] = useState(ITEMS);
+    const [value, setValue] = useState('');
+
+    function onItemChange (clickedItem) {
+        const changedCompleted = todo.map( item=>{
+            if(item.id===clickedItem.id){
+                item.completed=!item.completed;
+            }
+            return item;
+        });
+        setTodo(changedCompleted)
+    }
+
+    
+    function addList(e){
+        e.preventDefault();
+        const newObj = [
+            ...todo,
+            {
+                id: Date.now(),
+                title: value,
+                completed: false
+            }
+            
+        ]
+        setTodo(newObj);
+        setValue('')
+    }
+
+    return (
+            <div>
+                <form action="" onSubmit={addList}>
+                    <input type="text" value={value} onChange={e=>setValue(e.target.value)}/>
+                </form>
+                <ul>
+                    {todo.map(item=>{
+                        return <li key={item.id} className={classNames({completed: item.completed})}>
+                                    <input 
+                                        type="checkbox" 
+                                        checked={item.completed}
+                                        onChange={() => onItemChange(item)}
+                                    />
+                                    {item.title}
+                                    <button>delete</button>
+                                </li>
+                    })}
+                </ul>
+            </div>
+        )
 }
 
 export default TodoApp
